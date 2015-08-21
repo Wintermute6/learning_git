61ddb4048d933afd83e83e2bb9272de3ba9c3bb3
Wintermute6 <hannes.up@gmail.com>
2015-08-21 14:30:39 +0200

    added new package my.stuff and a class hello which contains no code

---
+++ CardReader/Application/src/main/java/my/stuff/hello.java
@@ -0,0 +1,15 @@
+package my.stuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hannes on 21.08.2015.
+ */
+public class hello extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //just testing no codeing
+    }
+}
